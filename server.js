67bc00d59baeb0019c10729f8b5cfd09fad1d77b
toum21 <pey2moo1@gmail.com>
2023-10-31 15:08:00 -0700

    created get rquests for notes and index

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+// import express, node, and file system
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const PORT = 3001;
+
+// create the port for the server
+const PORT = process.env.PORT || 3001;
+
+// initalize express
 const app = express();
+
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// stactic public folder
 app.use(express.static('public'));
 
+// Get request for start page
+app.get('/', (reg, res)=>{
+    res.sendFile(path,join(__dirname, '/public/index.html'))
+})
+
+// Get request for notes page
+app.get('/notes', (reg, res)=>{
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
 
 
 
@@ -16,34 +33,7 @@ app.use(express.static('public'));
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// listen for connections on specified ports
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
