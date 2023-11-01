b716af546892ba96364da451486c7f6dc1c8cccf
toum21 <pey2moo1@gmail.com>
2023-11-01 14:55:55 -0700

    created ids for every note created as well as delete functionality based off those ids

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const fs = require('fs');
 
+// function that generates a random number for the notes id 
+function randomID() {
+    return (Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1));
+}
+
 // all routes prepeneded with '/api'
 
 router.get('/notes', (req, res) => {
@@ -15,13 +22,26 @@ router.post('/notes', (req, res) => {
     const givenNotes = require('../db/db.json')
     const newNote = req.body
     givenNotes.push(newNote)
-
+    newNote.id = randomID()
     fs.writeFile('./db/db.json', JSON.stringify(givenNotes, null, 2), err => {
-        if (err) throw Error ('Oh No Something Went Wrong!')
+        if (err) throw Error('Oh No Something Went Wrong!')
         res.status(201).end();
     })
 })
 
+
+router.delete('/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        const deleteNotes = JSON.parse(data)
+        const updatedNotes = deleteNotes.filter(note => note.id !== req.params.id);
+
+        fs.writeFile('./db/db.json', JSON.stringify(updatedNotes, null, 2), err => {
+            if (err) { throw Error('Oh No Something Went Wrong!') } else { console.log('Note Deleted') }
+            res.json(updatedNotes)
+        })
+    })
+})
 
 
 module.exports = router;
